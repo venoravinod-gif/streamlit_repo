20f80666fb9c0331784e2d2bb1661ba8471f6eb6
venoravinod-gif <venoravinod@gmail.com>
2025-08-21 17:45:25 +0530

    Update title1 (1).py

--- title1 (1).py
+++ title1 (1).py
@@ -1,2 +1,7 @@
 import streamlit as st
 st.title("Welcome to the streamlit world!")
+st.header("this is a section heading")
+st.subheader("this is a sub heading")
+st.write("hello! This is my first streamlit app")
+st.markdown("hello this is bold/italic")
+st.caption("made by using streamlit")
