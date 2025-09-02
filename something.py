9ed638e15c6a76cf0404fa366e7f946846d2512c
venoravinod-gif <venoravinod@gmail.com>
2025-09-02 17:38:01 +0530

    Update something.py

--- something.py
+++ something.py
@@ -1,5 +1,18 @@
 #upload homework(only text files allowed")
 import streamlit as st
+
+import streamlit as staticmethod
+st.title("My file uploader playground")
+
+#upload homework(only text files allowed")
+hw_files = st.file_uploader("Upload your HW text files", type = ["txt","pdf"]
+                            , accept_multiple_files=True,key="homework")
+
+if hw_files:
+  st.write("Your HW files uploaded succesfully")
+  for i in hw_files:
+    st.write("----",i.name)
+    
 dr_files = st.file_uploader("Upload your DR files", type = ["png","jpg","jpeg"]
                             , accept_multiple_files=True,key="drawing")
 
