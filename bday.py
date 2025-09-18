3de770ba01732da3d1915074a00bf620eb72e175
venoravinod-gif <venoravinod@gmail.com>
2025-09-18 17:36:58 +0530

    Create bday.py

---
+++ bday.py
@@ -0,0 +1,29 @@
+#colors and formatting with st.markdown()
+import streamlit as st
+from datetime import date
+
+#title with style
+#st.markdown("**Happy Birthday**")#bold
+#st.markdown("_Happy Birthday_")#italic
+
+bday = st.date_input("Pick your birth date",
+    min_value = (1900,1,1),
+    max_value = (2025,12,31)
+)
+
+#ask for a fun message
+message = st.text_area("Write your bday message:")
+
+#button to create card
+if st.button("Make my Card!"):
+  if not message.strip():
+    st.error("oops! you forgot to type your message")
+  else:
+    #show card
+    st.markdown(f"""
+    <h2 style='color:purple;'> Happy Birthday!</h2> "
+    <p style = 'font-size:18px; color:black;'> Your Birthday is on: {bday}</p> 
+    <p style = 'font-size:18px;color:blue;'>  {message}</p> 
+    <p style = 'font-size:18px;color:green;'>  Have the best day ever</p> 
+    """, unsafe_allow_html=True)
+    st.success("Your bday card is ready!")
